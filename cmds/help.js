bc0f406a68983c4ea1f33ff769d8f36e3760ebe2
Working <tuy54190@gmail.com>
2026-07-20 22:21:47 -0600

    Update help.js

--- cmds/help.js
+++ cmds/help.js
@@ -14,7 +14,6 @@ module.exports = {
 				{ name: "/users <query>", value: "Check a player's profile by name or account ID" },
 				{ name: "/level <query>", value: "Check level info by ID or name" },
 				{ name: "/levelby <page> <user>", value: "List levels by a player" },
-				{ name: "/levelsearch <page> <query>", value: "Search levels on the GDPS" },
 				{ name: "/leveltop <levelid> [page]", value: "Leaderboard for a specific level" },
 				{ name: "/dailylevel", value: "Check today's daily level" },
 				{ name: "/weeklylevel", value: "Check this week's weekly level" },
